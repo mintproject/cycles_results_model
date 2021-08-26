13493a93a0c537d869cdeb95e406ed4ede925034
Varun Ratnakar <varunratnakar@gmail.com>
2021-08-26 17:13:21 +0530

    Modified to add dummy args

--- fetch_results.sh
+++ fetch_results.sh
@@ -6,4 +6,5 @@ WEED_FRACTION=$8
 
 HOST="https://cycles-results.mint.isi.edu"
 
+echo "date,country,admin1,admin2,admin3,crop_production,grain_yield"
 curl -s "$HOST/getCyclesResults/$CROP_NAME/$START_PLANTING_DAY/$FERTILIZER_RATE/$WEED_FRACTION"
