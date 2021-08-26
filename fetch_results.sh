c5d974370a3af0532093ef6749db93db2133e564
Varun Ratnakar <varunratnakar@gmail.com>
2021-08-26 13:51:07 +0530

    Added option for remote fetch

--- fetch_results.sh
+++ fetch_results.sh
@@ -6,4 +6,8 @@ WEED_FRACTION=$4
 
 OUTPUT_FILE=$5
 
-psql -d cycles -c "\copy (SELECT date,country,admin1,admin2,admin3,production,grain_yield FROM cycles_results WHERE crop_name='$CROP_NAME' AND start_planting_day=$START_PLANTING_DAY AND fertilizer_rate=$FERTILIZER_RATE AND weed_fraction='$WEED_FRACTION') TO '$OUTPUT_FILE' WITH CSV"
+DBHOST="cycles-results.mint.isi.edu"
+DBUSER="dojo"
+
+export PGPASSWORD="mellowcubicle"
+psql -U $DBUSER -h $DBHOST -d cycles -c "\copy (SELECT date,country,admin1,admin2,admin3,production,grain_yield FROM cycles_results WHERE crop_name='$CROP_NAME' AND start_planting_day=$START_PLANTING_DAY AND fertilizer_rate=$FERTILIZER_RATE AND weed_fraction='$WEED_FRACTION') TO '$OUTPUT_FILE' WITH CSV"
