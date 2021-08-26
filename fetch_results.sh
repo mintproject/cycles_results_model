db9138e08940b0e4c0cb20189e69e451afe1877e
Varun Ratnakar <varunratnakar@gmail.com>
2021-08-26 23:14:20 +0530

    Hardcoded output

--- fetch_results.sh
+++ fetch_results.sh
@@ -6,7 +6,8 @@ WEED_FRACTION=$8
 
 HOST="https://cycles-results.mint.isi.edu"
 
-mkdir -p outputs
+#mkdir -p outputs
+sleep 5
 
 echo "date,country,admin1,admin2,admin3,crop_production,grain_yield" > outputs/cycles-output.csv
 curl -s "$HOST/getCyclesResults/$CROP_NAME/$START_PLANTING_DAY/$FERTILIZER_RATE/$WEED_FRACTION" >> outputs/cycles-output.csv
